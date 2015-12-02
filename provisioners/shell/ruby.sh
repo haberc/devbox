f427e9a5066400ad03f999681a44395e1eef13e6
Leon Weidauer <leon@lnwdr.de>
2015-12-02 16:19:55 +0100

    ruby gems

--- provisioners/shell/ruby.sh
+++ provisioners/shell/ruby.sh
@@ -1,15 +1,10 @@
 #install ruby
 
-# ruby-gems from ports depends on ruby21, so we install that
-
-if [ ! -f /usr/local/bin/ruby21 ] ; then
-    pkg install -y ruby21
-fi
-
 if [ ! -L /usr/local/bin/ruby ] ; then
-    ln -s /usr/local/bin/ruby21 /usr/local/bin/ruby
+    pkg install -y ruby
 fi
 
-if [ ! -f /usr/local/bin/gem ] ; then
-    # TODO: install gem without compiling an entirely new ruby
+if [ ! -L /usr/local/bin/gem ] ; then
+    cd /usr/ports/devel/ruby-gems
+    make install clean
 fi
